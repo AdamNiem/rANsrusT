540a9fd018d1983ef5bb66c132ade58650184f21
Ethan <ethanhmarquez@gmail.com>
2026-04-05 16:17:47 -0400

    working enwik8 test

--- data.sh
+++ data.sh
@@ -3,3 +3,7 @@
 wget http://mattmahoney.net/dc/enwik8.zip
 unzip enwik8.zip
 rm enwik8.zip
+
+wget https://g-8d6b0.fd635.8443.data.globus.org/ds131.2/Data-Reduction-Repo/raw-data/EXASKY/NYX/SDRBENCH-EXASKY-NYX-512x512x512.tar.gz
+tar -xzvf SDRBENCH-EXASKY-NYX-512x512x512.tar.gz
+rm SDRBENCH-EXASKY-NYX-512x512x512.tar.gz
